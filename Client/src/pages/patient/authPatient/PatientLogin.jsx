cf94a7691251250f73ce3d4798df160cde8bfe21
Ujjwal patil <ujjwalpatil0011.55@gmail.com>
2025-08-12 15:27:58 +0530

    ujjwal commit

---
+++ Client/src/pages/patient/authPatient/PatientLogin.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PatientLogin() {
+  return <div>PatientLogin</div>;
+}
--- Server/config/initDB.js
+++ Server/config/initDB.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config()
 const dbUrl = process.env.MONGODB_URL;
 
 export const connectDB = async () => {
